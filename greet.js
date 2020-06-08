c3f0af4fd9e56142047d9639724d3486488669de
Loreen-netizen <loreenmuusha@gmail.com>
2020-06-08 16:44:56 +0200

    click event listener added

---
+++ greet.js
@@ -0,0 +1,9 @@
+var greetBtnClicked = function(){
+
+    var theName = document.getElementById("theInputTag")
+  var greetbtn =  document.getElementById("greetbtn");
+
+
+ alert("Hello " + theInputTag.value);
+}
+greetbtn.addEventListener("click", greetBtnClicked)
