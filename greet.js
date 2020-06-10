a5d2c7426c95a80cf2c36dd6db6eb3b8ad560d68
Loreen-netizen <loreenmuusha@gmail.com>
2020-06-10 16:51:10 +0200

    javascript for clearing text box added

--- greet.js
+++ greet.js
@@ -1,15 +1,23 @@
 var theName = document.getElementById("theInputTag")
-var greetbtn =  document.getElementById("greetbtn");
+var greetbtn = document.getElementById("greetbtn");
 var greetLabel = document.createElement("label")
 var parentContainer = document.getElementsByClassName("container");
 var greetBtnClicked = function(){
-var containerDiv = document.createElement("containerDiv")
-containerDiv.className = "greetLabelDiv"
-greetLabel.id = "theGreetLabel"
-greetLabel.innerHTML = ("Hello " + theName.value);
-containerDiv.appendChild(greetLabel);
-// document.parentContainer.appendChild(containerDiv);
 
-document.body.appendChild(containerDiv);
+  var containerDiv = document.createElement("containerDiv")
+  containerDiv.className = "greetLabelDiv"
+  greetLabel.id = "theGreetLabel"
+  greetLabel.innerHTML = ("Hello " + theName.value);
+  containerDiv.appendChild(greetLabel);
+  // document.parentContainer.appendChild(containerDiv);
+
+  document.body.appendChild(containerDiv);
 }
-greetbtn.addEventListener("click", greetBtnClicked)
+var clearGreetInput = function(){
+theName.value = "";
+}
+
+
+
+greetbtn.addEventListener("click", greetBtnClicked);
+greetbtn.addEventListener("click", clearGreetInput);
