008897e4f82119165bbd5c99f97626d56d996d69
Loreen-netizen <loreenmuusha@gmail.com>
2020-06-19 13:19:26 +0200

    counter reset nad language radio buttons added

--- greet.js
+++ greet.js
@@ -3,67 +3,101 @@ var greetbtn = document.getElementById("greetbtn");
 var greetLabel = document.getElementById("theGreetLabel")
 var counterLabel = document.getElementById("counterLabel")
 var parentContainer = document.getElementsByClassName("container");
+var languageLabels = document.getElementsByName("language");
+var  resetCounter = document.getElementById("resetCounterButton")
+
 var theNamesArray = [];
-var counter= 0;
+var counter = 0;
+var languageChosen = "";
 
-var pageRefreshedCount = function(){
-  if (JSON.parse(localStorage.getItem("totalPeopleGreeted"))=== null){
-     counter = 0;
+var pageRefreshedCount = function () {
+  if (JSON.parse(localStorage.getItem("totalPeopleGreeted")) === null) {
+    counter = 0;
   }
-  else 
-
-  {
-     counter = JSON.parse(localStorage.getItem("totalPeopleGreeted"))
-}
+  else {
+    counter = JSON.parse(localStorage.getItem("totalPeopleGreeted"))
+  }
 }
 
-var pageRefreshedNames = function(){
-  if (JSON.parse(localStorage.getItem("namesArray")) === null){
+var pageRefreshedNames = function () {
+  if (JSON.parse(localStorage.getItem("namesArray")) === null) {
     theNamesArray = []
   }
-  else{
+  else {
     theNamesArray = JSON.parse(localStorage.getItem("namesArray"))
   }
 }
 
-var greetBtnClicked = function () {
-  if(theName.value === ""){
-    return
-  } 
- else{greetLabel.innerHTML = ("Hello " + theName.value);
-}
+var theLanguageChosen = function () {
+  let languageArray = Array.from(languageLabels);
+  for (let i = 0; i < languageArray.length; i++) {
+    if (languageArray[i].checked) {
+      // alert(languageArray[i].value)
+      languageChosen = languageArray[i].value;
+    }
 
-}
-
-var namesCounter = function(){
-  if(theName.value === ""){
-    return
-  } 
-  else{ counter++
-    
-    return counterLabel.innerHTML = ("total people greeted = " + counter);
   }
 }
 
-var clearGreetInput = function () {
-  theName.value = "";
-}
+  var greetBtnClicked = function () {
+    if (theName.value === "") {
+      return
+    }
+     if (languageChosen === "Shona") {
+    greetLabel.innerHTML = ("Hesi Kani " + theName.value);
+    }
+    if (languageChosen === "Ndebele") {
+    greetLabel.innerHTML = ("Sawubona " + theName.value);
+    }
+     if (languageChosen === "English") {
+      greetLabel.innerHTML = ("Hello " + theName.value);
+      }
 
-var storeNames = function () {
-
-  let allNames = {
-    "name": theName.value,
   }
-  theNamesArray.push(allNames);
 
-localStorage.setItem("namesArray", JSON.stringify(theNamesArray));
-localStorage.setItem("totalPeopleGreeted", JSON.stringify(counter));
-};
+  var namesCounter = function () {
+    if (theName.value === "") {
+      return
+    }
+    else {
+      counter++
 
-greetbtn.addEventListener("click", pageRefreshedCount)
-greetbtn.addEventListener("click", pageRefreshedNames)
-greetbtn.addEventListener("click", greetBtnClicked);
-greetbtn.addEventListener("click", namesCounter);
-greetbtn.addEventListener("click", storeNames);
+      return counterLabel.innerHTML = ("total people greeted = " + counter);
+    }
+  }
 
-greetbtn.addEventListener("click", clearGreetInput);
+  var clearGreetInput = function () {
+    theName.value = "";
+  }
+
+  var storeNames = function () {
+
+    let allNames = {
+      "name": theName.value,
+    }
+    theNamesArray.push(allNames);
+
+    localStorage.setItem("namesArray", JSON.stringify(theNamesArray));
+    localStorage.setItem("totalPeopleGreeted", JSON.stringify(counter));
+  };
+
+  var resetTheCounter = function(){
+    localStorage.removeItem("totalPeopleGreeted");
+    if (JSON.parse(localStorage.getItem("totalPeopleGreeted")) === null) {
+      counter = 0;
+    }
+    else {
+      counter = JSON.parse(localStorage.getItem("totalPeopleGreeted"))
+    }
+   return counterLabel.innerHTML = ("total people greeted = " + counter);
+  };
+
+  greetbtn.addEventListener("click", pageRefreshedCount);
+  greetbtn.addEventListener("click", pageRefreshedNames);
+  greetbtn.addEventListener("click", theLanguageChosen);
+  greetbtn.addEventListener("click", greetBtnClicked);
+  greetbtn.addEventListener("click", namesCounter);
+  greetbtn.addEventListener("click", storeNames);
+  greetbtn.addEventListener("click", clearGreetInput);
+  resetCounter.addEventListener("click", resetTheCounter);
+  // resetCounter.addEventListener("click", namesCounter);
