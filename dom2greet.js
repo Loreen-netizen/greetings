080d42046caa2f9857f67d923563bea2633423b6
Loreen-netizen <loreenmuusha@gmail.com>
2020-07-08 21:27:38 +0200

    retesting greet widget factory function and code refactor

--- dom2greet.js
+++ dom2greet.js
@@ -5,7 +5,6 @@ var counterLabel = document.getElementById("counterLabel")
 var parentContainer = document.getElementsByClassName("container");
 var languageLabels = document.getElementsByName("language");
 var resetCounter = document.getElementById("resetCounterButton");
-// var languageChosen = "";
 var getNames;
 if (localStorage['namesGreeted']) {
   getNames = JSON.parse(localStorage.getItem("namesGreeted"));
@@ -26,7 +25,6 @@ var greetBtnClicked = function () {
 var  nameStr = String(theName.value);
   if (checkedRadioBtn && theName.value !== "") {
     var lang = checkedRadioBtn.value
-    // billItemType will be 'call' or 'sms'
     greetingsFunctionInstance.verifyNames(nameStr)
     counterLabel.innerHTML = greetingsFunctionInstance.numberOfPeopleGreeted()
 
@@ -43,6 +41,11 @@ var  nameStr = String(theName.value);
   }
 }
 
+var clearGreetInput = function () {
+  theName.value = "";
+ };
+
+
 var clearLocalStorage = function () {
   localStorage.clear()
   location.reload();
@@ -54,115 +57,5 @@ window.setTimeout('onload', function () {
 }, 500)
 
 resetCounter.addEventListener('click', clearLocalStorage)
-greetbtn.addEventListener('click', greetBtnClicked)
-// resetCounter.addEventListener("click", resetTheCounter);
-// resetCounter.addEventListener("click", clearLocalStorage);
-
-
-
-
-// var pageRefreshedCount = function () {
-//   var getNames =JSON.parse(localStorage.getItem("namesGreeted"));
-//   if( Array.from(getNames) === null) {
-//     greetingsFunctionInstance.verifyNames(theName.value, Array.from(getNames));
-//   }
-//   else {
-//     counterLabel.innerHTML = greetingsFunctionInstance.numberOfPeopleGreeted();
-//     greetingsFunctionInstance.alreadyGreeted(theName.value, Array.from(getNames));
-//   }
-// }
-
-// var pageRefreshedNames = function () {
-//   if (JSON.parse(localStorage.getItem("namesGreeted")) === null) {
-//     namesGreeted = []
-//   }
-//   else {
-//     namesGreeted = JSON.parse(localStorage.getItem("namesGreeted"))
-//     greetingsFactoryFunction(namesGreeted);
-//   }
-// }
-
-// var theLanguageChosen = function(){
-//   let languageArray = Array.from(languageLabels);
-//   for (let i = 0; i < languageArray.length; i++) {
-//     if (languageArray[i].checked) {
-//        languageChosen = languageArray[i].value;
-//       greetBtnClicked(languageChosen);
-//       return languageChosen;
-//     }
-//     else{
-//       continue
-//     }
-
-//   }
-
-// }
-
-// var greetBtnClicked = function () {
-//   var getNames =JSON.parse(localStorage.getItem("namesGreeted"));
-//   if (languageChosen === undefined || languageChosen === "") {
-//     greetLabel.innerHTML = greetingsFunctionInstance.errorMessageLanguage();
-//   }
-//   else if(greetingsFunctionInstance.alreadyGreeted(theName.value, Array.from(getNames))=== true){
-//    return greetLabel.innerHTML = greetingsFunctionInstance.alreadyGreeted((theName.value, Array.from(getNames)));
-//   }
-
-//   else {
-//     greetLabel.innerHTML = greetingsFunctionInstance.greetLanguage(String(languageChosen), String(theName.value));
-//    return greetingsFunctionInstance.verifyNames(theName.value, Array.from(getNames));
-//   };
-
-// };
-// greetbtn.onclick = function () {
-//   theLanguageChosen();
-//   storeNames();
-//   pageRefreshedCount();
-//   pageRefreshedNames();
-//   greetBtnClicked();
-//   clearGreetInput();
-//   namesCounter();
-
-
-
-// }
-
-// var namesCounter = function () {
-//   var numberOfGreetings = greetingsFunctionInstance.numberOfPeopleGreeted();
-//   counterLabel.innerHTML = ("Total people greeted = " + numberOfGreetings);
-// }
-
-// var clearGreetInput = function () {
-//   theName.value = "";
-// }
-
-// var storeNames = function () {
-
-//   localStorage.setItem("namesGreeted", JSON.stringify(greetingsFunctionInstance.getName()));
-//   localStorage.setItem("numberOfPeopleGreeted", JSON.stringify(greetingsFunctionInstance.numberOfPeopleGreeted()));
-// };
-
-// var resetTheCounter = function () {
-//   localStorage.removeItem("namesGreeted");
-//   var counter = greetingsFunctionInstance.numberOfPeopleGreeted()
-//   if (JSON.parse(localStorage.getItem("namesGreeted")) === null) {
-//     counter = 0;
-//   }
-//   else {
-//     counter = JSON.parse(localStorage.getItem("namesGreeted"))
-//   }
-//   return counterLabel.innerHTML = ("total people greeted = " + counter);
-// };
-
-// var clearLocalStorage = function () {
-//   localStorage.removeItem("numberOfPeopleGreeted");
-//   greetLabel.innerHTML = ("welcome!");
-
-// }
-
-// window.onload = function () {
-//   var allNamesInArray = JSON.parse(localStorage.getItem("namesArray"))
-//   if (JSON.parse(localStorage.getItem("totalPeopleGreeted")) != null) {
-//     counterLabel.innerHTML = ("total people greeted = " + allNamesInArray.length);
-//   }
-//   else { counterLabel.innerHTML = "counter" }
-// };
+greetbtn.addEventListener('click', greetBtnClicked);
+greetbtn.addEventListener("click", clearGreetInput);
--- greet.tests/greetings.tests.js
+++ greet.tests/greetings.tests.js
@@ -1,72 +1,57 @@
 describe("greetingsFactoryFunction", function () {
 
-    it("should greet Yeukai", function () {
-
-        var greetFactoryFunction = greetingsFactoryFunction();
-
-        assert.equal("Hello Yeukai", greetFactoryFunction.greet("Yeukai"))
-    });
 
     it("should return 1 when one person is greeted", function () {
 
-        var greetFactoryFunction2 = greetingsFactoryFunction();
-        var peopleGreeted = greetFactoryFunction2.verifyNames("Loreen")
-        assert.equal(1, greetFactoryFunction2.numberOfPeopleGreeted(peopleGreeted))
+        var greetFactoryFunction1 = greetingsFactoryFunction();
+        var peopleGreeted = greetFactoryFunction1.verifyNames("Loreen")
+        assert.equal(1, greetFactoryFunction1.numberOfPeopleGreeted(peopleGreeted))
     });
 
     it("should greet people in 3 different languages", function () {
 
+        var greetFactoryFunction2 = greetingsFactoryFunction();
+
+        assert.equal("Hesi Kani Yeu!", greetFactoryFunction2.greetLanguage("Yeu", "Shona"));
+        assert.equal("Hello Hlosani!", greetFactoryFunction2.greetLanguage("Hlosani", "English"));
+        assert.equal("Sawubona Joey!", greetFactoryFunction2.greetLanguage("Joey", "Ndebele"))
+    });
+
+    it("should push a new username into an array", function () {
+
+        var greetFactoryFunction3 = greetingsFactoryFunction();
+        assert.deepEqual(["Titi"], greetFactoryFunction3.verifyNames("Titi"))
+    });
+
+    it("should return error message if language is not selected", function () {
+
         var greetFactoryFunction4 = greetingsFactoryFunction();
-
-        assert.equal("Hesi Kani ", greetFactoryFunction4.greetLanguage("Shona"));
-        assert.equal("Hello ", greetFactoryFunction4.greetLanguage("English"));
-        assert.equal("Sawubona ", greetFactoryFunction4.greetLanguage("Ndebele"))
+        assert.equal("Please select language", greetFactoryFunction4.errorMessageLanguage())
     });
 
-    it("should keep names in local storage", function () {
 
-        var greetFactoryFunction12 = greetingsFactoryFunction();
-        assert.equal("[]", greetFactoryFunction12.storeNames("[]"))
-    });
-
-    it("should not count the same person twice", function () {
-
+    it("should return an array with all usernames", function(){
         var greetFactoryFunction5 = greetingsFactoryFunction();
-        greetFactoryFunction5.greet("Loreen");
-        assert.equal("you have already been counted", greetFactoryFunction5.verifyFunction((["Yeu" , "Pru", "Rose"])))
-    });
-
-
-    it("should clear input text when greet button is clicked", function(){
-        var greetFactoryFunction7 = greetingsFactoryFunction();
-        assert.equal("", greetFactoryFunction7.clearGreetInput("Yeukai"))
-        
+        assert.deepEqual([], greetFactoryFunction5.getName())
+       
 
     });
 
-    it("should be able to get names from local storage", function(){
-        var greetFactoryFunction8 = greetingsFactoryFunction();
-        assert.deepEqual([], greetFactoryFunction8.getNames())
-        
+    it("should be able to return number of people greeted", function(){
+        var greetFactoryFunction6 = greetingsFactoryFunction();
+        assert.equal(0, greetFactoryFunction6.numberOfPeopleGreeted())
+       
 
     });
-
-    it("should return error message when language isnt selected", function(){
-        var greetFactoryFunction9 = greetingsFactoryFunction();
-        
-        assert.equal("Please select language", greetFactoryFunction9.errorMessageLanguage(""))
-        
-
-    });
-    
+   
 
     it("should return error message when user name has not been entered", function(){
-        var greetFactoryFunction10 = greetingsFactoryFunction();
-        
-        assert.equal("Please enter userName", greetFactoryFunction10.errorMessageUserName(""))
-        
+        var greetFactoryFunction7 = greetingsFactoryFunction();
+       
+        assert.equal("Please enter userName", greetFactoryFunction7.errorMessageUserName(""))
+       
 
     });
-    
+   
 
 });
