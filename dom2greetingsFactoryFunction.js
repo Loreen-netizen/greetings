19e0daf55e553a7e7929c98639d4e90a2b066a02
Loreen-netizen <loreenmuusha@gmail.com>
2020-07-09 10:30:02 +0200

    unnecessary files removed

--- dom2greetingsFactoryFunction.js
+++ dom2greetingsFactoryFunction.js
@@ -1,74 +1,62 @@
 var greetingsFactoryFunction = function(names) {
+    var language = undefined;
+   var namesGreeted = names || [];
 
-    // var greetingsCounter = 0;
-     var language = undefined;
-    var namesGreeted = names || [];
-    // var theName = "";
+   var verifyNames = function(name) {
+     var  theName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    
+     if(theName){
+       if (!namesGreeted.includes(theName)) {
+           namesGreeted.push(theName)
+           return namesGreeted;
+       }
+   }
+   };
 
-    var verifyNames = function(name) {
-      var  theName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-      
-      if(theName){
-        if (!namesGreeted.includes(theName)) {
-            namesGreeted.push(theName)
-        }
-    }
-    };
-
-    // var alreadyGreeted = function(theName,getNames) {
-    //     if (getNames.includes(theName.toUpperCase())) {
-    //         return "you have already been greeted"
-
-    //     }
-
-    // };
-
-    var greetLanguage = function (name, language) {
-       var caseName =  name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-        if (language === "Shona") {
-            return ("Hesi Kani " + caseName + "!")
-        }
-        if (language === "Ndebele") {
-            return ("Sawubona " + caseName + "!")
-        }
-        if (language === "English") {
-            return ("Hello " + caseName + "!")
-        }
-        else if(language === undefined || language === null){
-          return "Please select language!"
-        }
-    }
+   var greetLanguage = function (name, language) {
+      var caseName =  name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+       if (language === "Shona") {
+           return ("Hesi Kani " + caseName + "!")
+       }
+       if (language === "Ndebele") {
+           return ("Sawubona " + caseName + "!")
+       }
+       if (language === "English") {
+           return ("Hello " + caseName + "!")
+       }
+       else if(language === undefined || language === null){
+         return "Please select language!"
+       }
+   }
 
 
-    var errorMessageLanguage = function () {
-        if (language === null || language === undefined) {
-            return "Please select language"
-        }
+   var errorMessageLanguage = function () {
+       if (language === null || language === undefined) {
+           return "Please select language"
+       }
 
-    };
+   };
 
-    var getName = function () {
-        return namesGreeted;
-    };
+   var getName = function () {
+       return namesGreeted;
+   };
 
-    var errorMessageUserName = function (name) {
-        tName = name;
-        if (tName === "") {
-            return "Please enter userName"
-        }
-    }
+   var errorMessageUserName = function (name) {
+       if (name === "") {
+           return "Please enter userName"
+       }
+   }
 
-    var numberOfPeopleGreeted = function () {
-        return namesGreeted.length;
-    }
+   var numberOfPeopleGreeted = function () {
+       return namesGreeted.length;
+   }
 
-    return {
-        numberOfPeopleGreeted,
-        greetLanguage,
-        verifyNames,
-        errorMessageLanguage,
-        errorMessageUserName,
-        getName,
-        // alreadyGreeted ,
-    }
+   return {
+       numberOfPeopleGreeted,
+       greetLanguage,
+       verifyNames,
+       errorMessageLanguage,
+       errorMessageUserName,
+       getName,
+   }
 };
